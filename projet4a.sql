2e2e1d7ba335724f460bdb2c908dad1d7695c920
Arbii98 <arbisaidi8@gmail.com>
2022-03-26 21:13:01 +0100

    Latest DB

--- projet4a.sql
+++ projet4a.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 17 jan. 2022 à 14:09
+-- Généré le :  sam. 26 mars 2022 à 20:11
 -- Version du serveur :  5.7.19
 -- Version de PHP :  5.6.31
 
@@ -55,7 +55,17 @@ CREATE TABLE IF NOT EXISTS `convention` (
   PRIMARY KEY (`Identifiant_conv`),
   KEY `fk_id_entreprise_conv` (`Identifiant_entreprise`),
   KEY `fk_id_stage_conv` (`Identifiant_stage`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `convention`
+--
+
+INSERT INTO `convention` (`Identifiant_conv`, `Nom_entreprise_signataire_conv`, `Prénom_entreprise_signataire_conv`, `Adresse_envoi_conv`, `Identifiant_entreprise`, `Identifiant_stage`) VALUES
+(3, 'Signataire', 'Signataire', 'adresse statique', 24, 8),
+(4, 'ENT', 'ENT', 'adresse statique', 25, 8),
+(5, 'ENT', 'ENT', 'adresse statique', 26, 8),
+(6, 'ENT', 'ENT', 'adresse statique', 27, 9);
 
 -- --------------------------------------------------------
 
@@ -76,14 +86,17 @@ CREATE TABLE IF NOT EXISTS `entreprise` (
   `cp` varchar(30) NOT NULL,
   `ville` varchar(30) NOT NULL,
   PRIMARY KEY (`Identifiant_entreprise`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `entreprise`
 --
 
 INSERT INTO `entreprise` (`Identifiant_entreprise`, `Nom_entreprise`, `SIRET_entreprise`, `NAF_APE_entreprise`, `Nom_dirigeant_entreprise`, `Prenom_dirigeant_entreprise`, `Email_entreprise`, `rue`, `cp`, `ville`) VALUES
-(10, 'Sopra Steria', '1234', '5678', 'Saidi', 'Mohamed', 'arbisaidi8@gmail.com', '16 boulevard Charles Nicolle', '72000', 'Le Mans');
+(24, 'Sopra', '1234', '5678', 'Mohamed', 'Saidi', 'arbisaidi8@gmail.com', '16 boulevard Charles Nicolle', '72000', 'Le Mans'),
+(25, 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT'),
+(26, 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT'),
+(27, 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT');
 
 -- --------------------------------------------------------
 
@@ -105,14 +118,16 @@ CREATE TABLE IF NOT EXISTS `etudiant` (
   `nom_entreprise` varchar(30) DEFAULT NULL,
   `email_entreprise` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `etudiant`
 --
 
 INSERT INTO `etudiant` (`id`, `Num_etudiant`, `Nom_etudiant`, `Prenom_etudiant`, `Date_naissance_etudiant`, `Adresse_etudiant`, `Tel_etudiant`, `Email_etudiant`, `Attestation_url`, `nom_entreprise`, `email_entreprise`) VALUES
-(1, 'e2105465', 'Saidi', 'Mohamed', '1998-10-15', '16 boulevard Charles Nicolle', '0668259886', 'arbisaidi8@gmail.com', NULL, 'Test', 'Test@gmail.com');
+(1, 'e2105465', 'Saidi', 'Mohamed', '1998-10-15', '16 boulevard Charles Nicolle', '0668259886', 'arbisaidi8@gmail.com', NULL, 'Test', 'Test@gmail.com'),
+(2, '1234', 'Saidi', 'Arbi', '2022-03-09', '16 boulevard Charles Nicolle', '0668259886', 'arbisaidi8@gmail.com', NULL, 'Arbi Saidi', 'arbisaidi8@gmail.com'),
+(3, 'e2105465', 'Doe', 'John', NULL, NULL, NULL, NULL, NULL, 'Test', 'Test@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -132,14 +147,21 @@ CREATE TABLE IF NOT EXISTS `maitre_de_stage` (
   `Identifiant_entreprise` int(6) NOT NULL,
   PRIMARY KEY (`Identifiant_super`),
   KEY `fk_id_entreprise_maitre` (`Identifiant_entreprise`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `maitre_de_stage`
 --
 
 INSERT INTO `maitre_de_stage` (`Identifiant_super`, `Nom_super`, `Prenom_super`, `Statut_super`, `Poste_occupe`, `Tel_super`, `Email_super`, `Identifiant_entreprise`) VALUES
-(4, 'John', 'Doe', 'Indiv', 'Chef projet', '0668259886', 'arbisaidi8@gmail.com', 10);
+(30, 'Maitre1', 'Maitre1', 'Maitre1', 'Maitre1', 'Maitre1', 'Maitre1', 24),
+(31, 'Maitre2', 'Maitre2', 'Maitre2', 'Maitre2', 'Maitre2', 'Maitre2', 24),
+(32, 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 25),
+(33, 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 25),
+(34, 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 26),
+(35, 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 26),
+(36, 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 27),
+(37, 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 'ENT', 27);
 
 -- --------------------------------------------------------
 
@@ -152,13 +174,13 @@ CREATE TABLE IF NOT EXISTS `stage` (
   `Identifiant_stage` int(6) NOT NULL AUTO_INCREMENT,
   `Titre_stage` varchar(255) NOT NULL,
   `Description_stage` varchar(1000) NOT NULL,
-  `Date_début_stage` date NOT NULL,
+  `Date_debut_stage` date NOT NULL,
   `Date_fin_stage` date NOT NULL,
-  `Nb_heures_semaine_stage` int(11) NOT NULL,
+  `Nb_heures_semaine_stage` varchar(11) NOT NULL,
   `Covid_19_engagement_stage` int(11) NOT NULL,
   `Adresse_stage` varchar(500) NOT NULL,
   `IDE_stage` varchar(255) NOT NULL,
-  `Gratification_stage` int(6) NOT NULL,
+  `Gratification_stage` varchar(6) NOT NULL,
   `Avantage_stage` varchar(100) DEFAULT NULL,
   `id_etudiant` int(6) NOT NULL,
   `Identifiant_entreprise` int(6) NOT NULL,
@@ -167,7 +189,15 @@ CREATE TABLE IF NOT EXISTS `stage` (
   KEY `fk_id_etudiant` (`id_etudiant`),
   KEY `fk_id_entreprise` (`Identifiant_entreprise`),
   KEY `fk_id_tuteur` (`Identifiant_tuteur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `stage`
+--
+
+INSERT INTO `stage` (`Identifiant_stage`, `Titre_stage`, `Description_stage`, `Date_debut_stage`, `Date_fin_stage`, `Nb_heures_semaine_stage`, `Covid_19_engagement_stage`, `Adresse_stage`, `IDE_stage`, `Gratification_stage`, `Avantage_stage`, `id_etudiant`, `Identifiant_entreprise`, `Identifiant_tuteur`) VALUES
+(8, 'Stage dev', 'DevOps', '2022-02-25', '2022-03-12', '35', 1, '2 Impass Canal, rue MB, Bardo', 'IDE2', '1000', 'Transport', 1, 24, 1),
+(9, 'Stage Reseaux', 'ENT', '2022-03-08', '2022-03-25', '22', 1, 'ENT', 'ENT', '3', 'ENT', 2, 27, NULL);
 
 -- --------------------------------------------------------
 
@@ -186,7 +216,7 @@ CREATE TABLE IF NOT EXISTS `token` (
   PRIMARY KEY (`id`),
   KEY `fk_id_etudiant_token` (`id_etudiant`),
   KEY `fk_id_entreprise_token` (`id_entreprise`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `token`
@@ -194,7 +224,10 @@ CREATE TABLE IF NOT EXISTS `token` (
 
 INSERT INTO `token` (`id`, `token`, `validerEtudiant`, `validerEntreprise`, `id_etudiant`, `id_entreprise`) VALUES
 (18, 'TcQ1aI8mXo', 1, 0, 1, NULL),
-(19, 'wdbtEsuPxr', 1, 0, 1, NULL);
+(19, 'wdbtEsuPxr', 0, 1, 2, 27),
+(20, 'CbMyglXa8z', 0, 0, 1, NULL),
+(21, 'ySrhQucJP0', 0, 0, 1, NULL),
+(22, 'Q4RjAxiDst', 0, 0, 3, NULL);
 
 -- --------------------------------------------------------
 
@@ -209,7 +242,14 @@ CREATE TABLE IF NOT EXISTS `tuteur` (
   `Prenom_tuteur` varchar(100) NOT NULL,
   `Email_tuteur` varchar(100) NOT NULL,
   PRIMARY KEY (`Identifiant_tuteur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `tuteur`
+--
+
+INSERT INTO `tuteur` (`Identifiant_tuteur`, `Nom_tuteur`, `Prenom_tuteur`, `Email_tuteur`) VALUES
+(1, 'Hamon', 'Ludovic', 'EMAIL');
 
 --
 -- Contraintes pour les tables déchargées
