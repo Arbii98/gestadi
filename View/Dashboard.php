fce7bcf266e99b26e8c8e0525c3df0d733374622
Mehdi Behira <mehdibehira@gmail.com>
2022-05-14 12:46:44 +0200

    sticky filtre

--- View/Dashboard.php
+++ View/Dashboard.php
@@ -152,13 +152,13 @@
 <form action="" method="GET">
 
 
-<div class="p-8 bg-white shadow-2xl" >
+<div class="p-8 bg-white shadow-2xl" id="menu_filtre" >
 <h3 class="text-center">Filtres Avancées :</h3>
 <br />
 
 <div  style="display:flex;justify-content:space-evenly;flex-wrap:wrap;flex-direction:row">
 
-<div >
+<div  >
   <?php   
   $cheked= [];
 
@@ -989,6 +989,31 @@ $(document).ready(function() {
 		});
 
 	}) 	</script>
+
+  <script>
+    (function() { 
+  // on cible l'objet nav
+  let objNav = document.querySelector("#menu_filtre");
+  // on mémorise la position de nav
+  let memoPositionNav = objNav.offsetTop;
+  function sticky(){
+    // position du curseur au scroll
+    var posCurseur = this.pageYOffset;
+    // je teste la différence de distance entre le scroll et nav
+    if(memoPositionNav-posCurseur<1){
+      objNav.style.position = "fixed";
+      objNav.style.top = 0;
+      objNav.style.zIndex = 999;
+    }
+    if(posCurseur<101){
+      objNav.style.position = "relative";
+    }
+  }
+  // evenement
+  window.addEventListener("scroll", sticky);
+})()
+
+    </script>
 
 
 
